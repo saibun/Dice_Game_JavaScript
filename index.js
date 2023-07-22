595de825d073839463906a5a6952ab2589fabd06
Saikat Chatterjee <csaikat66@gmail.com>
2023-07-22 21:17:33 +0530

    add type javascript, change winging announcement

--- index.js
+++ index.js
@@ -10,10 +10,14 @@ let randomImageSource2="images/dice"+randomNumber2+".png";
 image2.setAttribute("src", randomImageSource2);
 
 if(randomNumber1>randomNumber2){
-	document.querySelector("h1").innerHTML="Player 1 wins! &#128681";
+	document.querySelector(".player1").innerHTML="Player 1 wins! &#128681";
 }else if(randomNumber1<randomNumber2){
-	document.querySelector("h1").innerHTML="Player 2 wins! &#128681";
+	document.querySelector(".player2").innerHTML="Player 2 wins! &#128681";
 
 }else{
-	document.querySelector("h1").innerHTML="Draw!";
+	document.querySelector("h2").innerHTML="Draw!";
 }
+var typed = new Typed('#element', {
+      strings: ['Refresh again'],
+      typeSpeed: 50,
+    });
